f9763ff1116166292ff06119fe94b2fb753adb20
mascararivers <rivers@atomicmail.io>
2025-10-11 10:38:17 +0100

    Revert "fixed github workflow ownership"
    This reverts commit 1a248f5e7cfd443866402e94d7db1a157f5b27ac.

--- src/main/java/com/rivers/urantia/events/TickEvent.java
+++ src/main/java/com/rivers/urantia/events/TickEvent.java
@@ -1,12 +1,10 @@
 package com.rivers.urantia.events;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 
 public class TickEvent {
     @SubscribeEvent
     public void tick(PlayerTickEvent event) {
-        Minecraft mc = Minecraft.getMinecraft();
 
     }
 }
